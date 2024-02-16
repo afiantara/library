f19d3488442be3155b1f418bd7556e1846958576
afiantara <afiantara@gmail.com>
2024-02-16 21:58:07 +0900

    Add files via upload

--- individual_general_insurance.py
+++ individual_general_insurance.py
@@ -3,9 +3,9 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 import pandas as pd
 
 
-url_path = './ojk/(revisi) 3.1 s.d. 3.11 Tabel Keuangan Asuransi Jiwa 2022.xlsx'
+url_path = './ojk/2.1 s.d 2.9 Tabel Keuangan Asuransi Umum dan Reasuransi 2022.xlsx'
 pre_title = 'REKAPITULASI'
-
+db_name='ASURANSI_UMUM'
 def load():
     workbook = load_workbook(url_path)
     for sheet_name in workbook.sheetnames:
@@ -14,34 +14,36 @@ def load():
         #title = sheet_title.split(maxsplit=1)
         tablename=sheet_title.replace(' ','').replace('.','')
         tablename = ''.join([i for i in tablename if not i.isdigit()])
-        #print(tablename)
+        print(tablename)
+        
         if tablename=='Neraca':
             load_neraca(sheet_title,tablename)
         elif tablename=='Investasi':
             load_investasi(sheet_title,tablename)
+        elif tablename=='NonInvestasi':
+            load_non_investasi(sheet_title,tablename)    
         elif tablename=='HasilInvestasi':
             load_hasil_investasi(sheet_title,tablename)
-        elif tablename=='NeracaUL':
-            load_neraca_ul(sheet_title,tablename)   
-        elif tablename=='LabaRugi':
+        elif tablename=='StatNeraca':
+            load_stat_neraca(sheet_title,tablename)   
+        elif 'LABARUGI' in tablename.upper():
             load_rl(sheet_title,tablename)      
-        elif tablename=='LRUL':
-            load_rl_ul(sheet_title,tablename)  
-        elif 'StatALE' in tablename:
+        elif tablename=='StatLR':
+            load_stat_lr(sheet_title,'STATINCOME')  
+        elif 'ASET' in tablename.upper():
             load_StatALE(sheet_title,'StatALE')
-        elif 'StatRL' in tablename:
+        elif 'LR-' in tablename.upper():
             load_StatRL(sheet_title,'StatRL')    
-        elif 'INVCAD' in tablename.upper():
+        elif 'INVDANCAD-' in tablename.upper():
             load_INVCAD(sheet_title,'INVCAD')
-
 def load_INVCAD(sheet_name,table_name):
     import pandas as pd
     df = pd.read_excel(url_path,sheet_name=sheet_name)
-    columns = getcolumns(df,8)
+    columns = getcolumns(df,9,1)
     df= getdata(df,13,columns,1)
     columns=['REPORT_DATE','NAME OF COMPANIES','INVESTMENTS','TECHNICAL RESERVES','RATIO(TECHNICAL_RESERVES/INVESTMENTS)']
     df_new =pd.DataFrame(columns=columns) 
-    df_new['NAME OF COMPANIES']=df['NAME OF COMPANY']
+    df_new['NAME OF COMPANIES']=df['NAME OF COMPANIES']
     for year in range(2018,2023):
         date = getLastDateYear(year)
         df_new['REPORT_DATE']=date
@@ -50,16 +52,16 @@ def load_INVCAD(sheet_name,table_name):
         df_new['TECHNICAL RESERVES']=df[year].iloc[:,1]
         df_new['RATIO(TECHNICAL_RESERVES/INVESTMENTS)']=df[year].iloc[:,2]
         df_new=df_new.assign(PRICE_INDEX=1000000)
-        dump_to_db('ASURANSI',table_name.upper(),df_new,date)
+        dump_to_db(table_name.upper(),df_new,date)
                   
 def load_StatRL(sheet_name,table_name):
     import pandas as pd
     df = pd.read_excel(url_path,sheet_name=sheet_name)
-    columns = getcolumns(df,8)
-    df= getdata(df,13,columns,1)
+    columns = getcolumns(df,9,1)
+    df= getdata(df,14,columns,1)
     columns=['REPORT_DATE','NAME OF COMPANIES','INCOMES','EXPENSES','PROFIT(LOSS)']
     df_new =pd.DataFrame(columns=columns) 
-    df_new['NAME OF COMPANIES']=df['NAME OF COMPANY']
+    df_new['NAME OF COMPANIES']=df['NAME OF COMPANIES']
     for year in range(2018,2023):
         date = getLastDateYear(year)
         df_new['REPORT_DATE']=date
@@ -68,16 +70,16 @@ def load_StatRL(sheet_name,table_name):
         df_new['EXPENSES']=df[year].iloc[:,1]
         df_new['PROFIT(LOSS)']=df[year].iloc[:,2]
         df_new=df_new.assign(PRICE_INDEX=1000000)
-        dump_to_db('ASURANSI',table_name.upper(),df_new,date)
+        dump_to_db(table_name.upper(),df_new,date)
 
 def load_StatALE(sheet_name,table_name):
     import pandas as pd
     df = pd.read_excel(url_path,sheet_name=sheet_name)
-    columns = getcolumns(df,8)
-    df= getdata(df,13,columns,1)
+    columns = getcolumns(df,9,1)
+    df= getdata(df,11,columns,1)
     columns=['REPORT_DATE','NAME OF COMPANIES','ASSETS','LIABILITIES','EQUITIES']
     df_new =pd.DataFrame(columns=columns) 
-    df_new['NAME OF COMPANIES']=df['NAME OF COMPANY']
+    df_new['NAME OF COMPANIES']=df['NAME OF COMPANIES']
     for year in range(2018,2023):
         date = getLastDateYear(year)
         df_new['REPORT_DATE']=date
@@ -86,53 +88,59 @@ def load_StatALE(sheet_name,table_name):
         df_new['LIABILITIES']=df[year].iloc[:,1]
         df_new['EQUITIES']=df[year].iloc[:,2]
         df_new=df_new.assign(PRICE_INDEX=1000000)
-        dump_to_db('ASURANSI',table_name.upper(),df_new,date)
-
-def load_rl_ul(sheet_name,table_name):
-    df = pd.read_excel(url_path,sheet_name=sheet_name)
-    date = getReportDate(df,3)
-    columns = getcolumns(df,10)
-    arrcols = []
-    for column in columns[:-1]:
-        arrcols.append(column)
-    arrcols.append('INCREASE (DECREASE) IN ASSET')
-    columns=arrcols
-    df= getdata(df,13,columns,1)
-
-    df.insert(0,'REPORT_DATE',date)
-    df=df.assign(PRICE_INDEX=1000000)
-    dump_to_db('ASURANSI',table_name.upper(),df,date)
+        dump_to_db(table_name.upper(),df_new,date)
 
 def load_rl(sheet_name,table_name):
-    columns=['NAME OF COMPANY','NET EARNED PREMIUM','INVESTMENT YIELDS','OTHER INCOME','TOTAL INCOME',
-        'NET CLAIM EXPENSES','OPERATIONAL EXPENSES','OTHER EXPENSES','TOTAL EXPENSES','EARNING BEFORE TAX',
-        'TAX','EARNING AFTER TAX', 'OTHER COMPREHENSIVE INCOME','TOTAL OTHER COMPREHENSIVE INCOME']
+    import numpy as np
     df = pd.read_excel(url_path,sheet_name=sheet_name)
     date = getReportDate(df,3)
-    df= getdata(df,14,columns,1)
+    columns = getcolumns(df,8,1)
+    columns=np.insert(columns,0,'NAME OF COMPANIES')
+    df= getdata(df,13,columns,2)
     df.insert(0,'REPORT_DATE',date)
     df=df.assign(PRICE_INDEX=1000000)
-    dump_to_db('ASURANSI',table_name.upper(),df,date)
+    dump_to_db(table_name.upper(),df,date)
 
-def load_neraca_ul(sheet_name,table_name):
+def load_stat_lr(sheet_name,table_name):
+    import pandas as pd
     df = pd.read_excel(url_path,sheet_name=sheet_name)
-    date = getReportDate(df,3)
-    columns = getcolumns(df,10)
-    df= getdata(df,13,columns,1)
+    columns =['INCOME','2018','2019','2020','2021','2022']
+    df= getdata(df,9,columns,3)
+    print(df)
+    columns=['REPORT_DATE','INCOME','VALUES']
+    df_new =pd.DataFrame(columns=columns) 
+    df_new['INCOME']=df['INCOME']
+    for year in range(2018,2023):
+        date = getLastDateYear(year)
+        df_new['REPORT_DATE']=date
+        colYear = '{}'.format(year)
+        df_new['VALUES']=df[colYear]
+        df_new=df_new.assign(PRICE_INDEX=1000000)
+        dump_to_db(table_name.upper(),df_new,date)
 
-    df.insert(0,'REPORT_DATE',date)
-    df=df.assign(PRICE_INDEX=1000000)
-    dump_to_db('ASURANSI',table_name.upper(),df,date)
-
+def load_stat_neraca(sheet_name,table_name):
+    import pandas as pd
+    df = pd.read_excel(url_path,sheet_name=sheet_name)
+    columns =['ASSETS','2018','2019','2020','2021','2022']
+    df= getdata(df,9,columns,2)
+    columns=['REPORT_DATE','ASSETS','VALUES']
+    df_new =pd.DataFrame(columns=columns) 
+    df_new['ASSETS']=df['ASSETS']
+    for year in range(2018,2023):
+        date = getLastDateYear(year)
+        df_new['REPORT_DATE']=date
+        colYear = '{}'.format(year)
+        df_new['VALUES']=df[colYear]
+        df_new=df_new.assign(PRICE_INDEX=1000000)
+        dump_to_db(table_name.upper(),df_new,date)
 def load_hasil_investasi(sheet_name,table_name):
     df = pd.read_excel(url_path,sheet_name=sheet_name)
     date = getReportDate(df,3)
-    columns = getcolumns(df,8)
-    df= getdata(df,13,columns,1)
-
+    columns = getcolumns(df,7,1)
+    df= getdata(df,12,columns,1)
     df.insert(0,'REPORT_DATE',date)
     df=df.assign(PRICE_INDEX=1000000)
-    dump_to_db('ASURANSI',table_name.upper(),df,date)
+    dump_to_db(table_name.upper(),df,date)
 
 def load_investasi(sheet_name,table_name):
     df = pd.read_excel(url_path,sheet_name=sheet_name)
@@ -141,16 +149,25 @@ def load_investasi(sheet_name,table_name):
     df= getdata(df,13,columns)
     df.insert(0,'REPORT_DATE',date)
     df=df.assign(PRICE_INDEX=1000000)
-    dump_to_db('ASURANSI',table_name.upper(),df,date)
-    
+    dump_to_db(table_name.upper(),df,date)
+
+def load_non_investasi(sheet_name,table_name):
+    df = pd.read_excel(url_path,sheet_name=sheet_name)
+    date = getReportDate(df,3)
+    columns = getcolumnsMultiRows(df,10,1)
+    df= getdata(df,16,columns,1)
+    df.insert(0,'REPORT_DATE',date)
+    df=df.assign(PRICE_INDEX=1000000)
+    dump_to_db(table_name.upper(),df,date)
+
 def load_neraca(sheet_name,table_name):
     df = pd.read_excel(url_path,sheet_name=sheet_name)
     date = getReportDate(df,3)
-    columns = getcolumns(df,8)
-    df= getdata(df,13,columns)
+    columns = getcolumnssplit(df,7)
+    df= getdata(df,12,columns)
     df.insert(0,'REPORT_DATE',date)
     df=df.assign(PRICE_INDEX=1000000)
-    dump_to_db('ASURANSI',table_name.upper(),df,date)
+    dump_to_db(table_name.upper(),df,date)
 
 def getReportDate(df,start):
     import datetime
@@ -168,8 +185,25 @@ def getFirstDateYear(year):
     first_day_of_year = datetime.date.min.replace(year=year)
     return first_day_of_year    
 
-def getcolumns(df,start):
-    columns = df.iloc[start].dropna().values
+def getcolumnssplit(df,start):
+    df=df.iloc[start].dropna()
+    df = df.str.split('\n', expand=True)
+    df.columns = ['IN','EN']
+    columns = df['EN'].dropna().values
+    return columns
+
+def getcolumns(df,start_row,start_col=2):
+    columns = df.iloc[start_row,start_col:].dropna().values
+    return columns
+
+def getcolumnsMultiRows(df,start_row,start_col=2):
+    df1=df.iloc[start_row,start_col:]
+    df2=df.iloc[start_row+1,start_col:]
+    dfs = [df1,df2]
+    df = pd.concat(dfs,axis=1)
+    df.columns = ['col1','col2']
+    df['columns'] = df['col1'].map(str) + ' ' + df['col2'].fillna('').map(str)
+    columns = df['columns'].str.strip().dropna().values
     return columns
 
 def getdata(df,start_row,columns,start_col=2):
@@ -180,7 +214,7 @@ def getdata(df,start_row,columns,start_col=2):
     df=df[df[columns[0]].str.contains( "JUMLAH" )==False]
     return df
 
-def dump_to_db(db_name,table_name,df,report_date):
+def dump_to_db(table_name,df,report_date):
     from sqlalchemy import create_engine
     from sqlalchemy.exc import SQLAlchemyError
     db = create_engine('sqlite:///{}.db'.format(db_name))
@@ -189,7 +223,6 @@ def dump_to_db(db_name,table_name,df,report_date):
         with db.connect() as conn:
             query="DELETE FROM {} WHERE REPORT_DATE='{}'".format(table_name,report_date)
             r_set=conn.exec_driver_sql(query)
-            conn.commit()
     except SQLAlchemyError as e:
         error = str(e)
         print(error)
